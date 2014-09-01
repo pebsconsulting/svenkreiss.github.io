78c918b23a8696daaceab23002ac2a1f6220de11
Sven Kreiss <sk@svenkreiss.com>
2014-08-31 22:35:57 -0400

    enable travisci html5 check

---
+++ validate.py
@@ -0,0 +1,25 @@
+"""Validates HTML5 pages."""
+
+import sys
+import requests
+
+
+# Change to your username and project of the form
+# http://<username>.github.io/<project>/
+URL = 'http://www.svenkreiss.com/'
+
+
+def main():
+    r = requests.get('http://html5.validator.nu/', params={'doc': URL})
+    if 'The document is valid HTML5' in r.text:
+        # success. all good.
+        return
+
+    # there were problems. here is the output:
+    print r.text
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
+
